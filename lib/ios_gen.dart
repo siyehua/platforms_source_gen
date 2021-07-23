e00c2fc963afff4f0af8fc8614af914b6eebea9c
Dywanedu <dywanedu@tencent.com>
2021-07-23 17:50:54 +0800

    Feat($ios) : 添加dart转objective-c功能

--- lib/ios_gen.dart
+++ lib/ios_gen.dart
@@ -1,34 +1,51 @@
+import 'dart:developer';
 import 'dart:io';
 
-import 'method_parse.dart';
+import 'bean/method_parse.dart';
+import 'gen_file_edit.dart';
 import 'platforms_source_gen.dart';
-import 'property_parse.dart';
+import 'bean/property_parse.dart';
+import 'type_utils.dart';
 
 class ObjectiveCCreate {
   static const Map<String, String> baseTypeMap = {
     "dart.core.bool": "BOOL",
     "dart.core.int": "int",
     "dart.core.double": "double",
-    "dart.core.String": "String",
   };
   static const Map<String, String> classTypeMap = {
+    "dart.core.String": "String",
     "dart.typed_data.Uint8List": "NSArray",
     "dart.typed_data.Int32List": "NSArray",
     "dart.typed_data.Int64List": "NSArray",
     "dart.typed_data.Float64List": "NSArray",
     "dart.core.List": "NSArray",
     "dart.core.Map": "NSDictionary",
+    "void": "void",
+  };
+  static const Map<String, String> specialTypeMap = {
+    "dart.async.Future": "",
   };
 
-  static void create(String savePath, List<GenClassBean> genClassBeans) {
+  static var prefix = "";
+
+  static void create(
+      String projectPrefix, String savePath, List<GenClassBean> genClassBeans) {
+    createHeaderFile(projectPrefix, savePath, genClassBeans);
+    createImplementFile(projectPrefix, savePath, genClassBeans);
+  }
+
+  static void createHeaderFile(
+      String projectPrefix, String savePath, List<GenClassBean> genClassBeans) {
     Directory iosTargetDir = Directory(savePath);
+    prefix = projectPrefix;
     bool exists = iosTargetDir.existsSync();
     if (!exists) {
       iosTargetDir.createSync(recursive: true);
     }
     genClassBeans.forEach((value) {
-      File ocHeaderFile = File(savePath + "/" + value.classInfo.name + ".h");
-
+      File ocHeaderFile =
+          File(savePath + "/" + projectPrefix + value.classInfo.name + ".h");
       //package
       String allContent = "";
 
@@ -36,7 +53,8 @@ class ObjectiveCCreate {
       List<String> imports = [
         "#import <Foundation/Foundation.h>\n",
       ];
-      imports.addAll(value.imports);
+      imports
+          .addAll(value.imports.where((element) => !element.contains(".dart")));
       String importStr = imports
           .toString()
           .replaceAll("[", "")
@@ -52,9 +70,9 @@ class ObjectiveCCreate {
       //method
       String methodStr = method(value.methods);
 
-      String absStr = "@interface";
+      String absStr = "\n@interface";
       allContent +=
-          "${absStr} ${value.classInfo.name} : NSObject {\n$propertyStr ${methodStr} \n}";
+          "${absStr} ${projectPrefix}${value.classInfo.name} : NSObject \n\n$propertyStr\n${methodStr}\nNS_ASSUME_NONNULL_END";
       ocHeaderFile.writeAsStringSync(allContent);
       if (!ocHeaderFile.existsSync()) {
         //if not create use dart io, use shell
@@ -63,45 +81,183 @@ class ObjectiveCCreate {
     });
   }
 
+  static void createImplementFile(
+      String projectPrefix, String savePath, List<GenClassBean> genClassBeans) {
+    genClassBeans.forEach((value) {
+      File ocImplementFile =
+          File(savePath + "/" + projectPrefix + value.classInfo.name + ".m");
+      //package
+      String className = "${projectPrefix}${value.classInfo.name}";
+      String allContent = "#import \"$className.h\"\n";
+      allContent += "\nNS_ASSUME_NONNULL_BEGIN\n";
+      allContent += "@implementation $className";
+
+      //property
+      allContent += propertyImplementation(value, value.properties);
+
+      //method
+      allContent += methodImplementation(value.methods);
+
+      allContent += "\n@end\nNS_ASSUME_NONNULL_END";
+      ocImplementFile.writeAsStringSync(allContent);
+      if (!ocImplementFile.existsSync()) {
+        //if not create use dart io, use shell
+        _savePath(allContent, ocImplementFile.path);
+      }
+    });
+  }
+
   /// create property
   static String property(GenClassBean genBean, List<Property> properties) {
     String propertyStr = "";
     properties.forEach((property) {
-      String typeStr = getTypeStr(property, wantAddPre: true);
+      String typeStr = getPropertyStr(property);
+      String name = property.name;
+      propertyStr += "$typeStr$name;\n";
+    });
+    return propertyStr;
+  }
+
+  /// create method
+  static String method(List<MethodInfo> methods) {
+    String result = "";
+    methods.forEach((method) {
+      result += "- (" + getTypeString(method.returnType) + ")" + method.name;
+      String argType = "";
+      if (!method.args.isEmpty) {
+        method.args.forEach((arg) {
+          argType += getTypeString(arg) + arg.name + ", ";
+        });
+        if (argType.endsWith(", ")) {
+          //remove ", " ,because java method arg can't end with ", "
+          argType = argType.substring(0, argType.length - 2);
+        }
+        result += "(" + argType + ")";
+      }
+      result += ";\n";
+    });
+    return result;
+  }
+
+  static String propertyImplementation(
+      GenClassBean genBean, List<Property> properties) {
+    String propertyStr =
+        "\n- (instancetype)init\n{\n\tself = [super init];\n\tif (self) {\n";
+    properties.forEach((property) {
       String name = property.name;
       String defaultValue = property.defaultValue1;
       if (defaultValue == "null") {
-        //todo no way if the String == "null"; don't do it
         defaultValue = "";
       }
       if (defaultValue.isNotEmpty) {
-        if (property.type == "dart.core.int" &&
-            !defaultValue.endsWith("L") &&
-            !defaultValue.endsWith("l")) {
-          //if type is int, the java type is Long, so your should add 'L' to the default
-          defaultValue += "L";
-        } else if (property.type == "dart.core.String") {
-          //string should wrap with ""
-          defaultValue = "\"$defaultValue\"";
+        propertyStr += "\t\tself.$name = ";
+        if (property.type == "dart.core.String") {
+          defaultValue = "@\"$defaultValue\"";
         } else if (property.type == "dart.typed_data.Uint8List" ||
             property.type == "dart.typed_data.Int32List" ||
             property.type == "dart.typed_data.Int64List" ||
-            property.type == "dart.typed_data.Float64List") {
-          defaultValue = " new " +
-              (baseTypeMap[property.type] ?? "") +
-              "{ " +
-              defaultValue.replaceAll('[', '').replaceAll(']', '') +
-              " }";
-        } else if (property.type == "dart.core.List") {
-          defaultValue = "new ArrayList<>()";
+            property.type == "dart.typed_data.Float64List" ||
+            property.type == "dart.core.List") {
+          defaultValue = "[NSArray array]";
         } else if (property.type == "dart.core.Map") {
-          defaultValue = "new HashMap<>()";
+          defaultValue = "[NSDictionary dictionary]";
         }
-        defaultValue = " = " + defaultValue;
+        propertyStr += "$defaultValue;\n";
       }
-      propertyStr += "\t$typeStr $name $defaultValue;\n";
     });
+    propertyStr += "\n\t}\n\treturn self;\n}\n";
     return propertyStr;
+  }
+
+  static String methodImplementation(List<MethodInfo> methods) {
+    String result = "\n";
+    methods.forEach((method) {
+      result += "- (" + getTypeString(method.returnType) + ")" + method.name;
+      String argType = "";
+      if (!method.args.isEmpty) {
+        method.args.forEach((arg) {
+          argType += getTypeString(arg) + arg.name + ", ";
+        });
+        if (argType.endsWith(", ")) {
+          //remove ", " ,because java method arg can't end with ", "
+          argType = argType.substring(0, argType.length - 2);
+        }
+        result += "(" + argType + ")";
+      }
+      result += " {}\n";
+    });
+    return result;
+  }
+
+  /// cover dart type to objc type
+  static String getPropertyStr(
+    Property property, {
+    bool showNullTag = true,
+  }) {
+    String propertyString = "@property (nonatomic, ";
+    var baseType = baseTypeMap[property.type];
+    var classType = classTypeMap[property.type];
+    if (baseType != null) {
+      //base
+      propertyString += "assign) " + baseType + " ";
+    } else if (classType != null) {
+      propertyString += "strong";
+      if (showNullTag && propertyString != "void") {
+        propertyString +=
+            (property.canBeNull ? ", nullable) " : ") ") + classType;
+      }
+      if (property.subType.isNotEmpty) {
+        propertyString += getSubTypeString(property);
+      }
+      propertyString += " *";
+    } else {
+      //other not base type
+      propertyString = "${property.type.split(".").last} *";
+    }
+    return propertyString;
+  }
+
+  static String getSubTypeString(Property property) {
+    String subTypeString = "";
+    subTypeString += "<";
+    property.subType.forEach((element) {
+      subTypeString += getTypeString(element, convertToClass: true) + ", ";
+    });
+    if (subTypeString.endsWith(", ")) {
+      //remove ", " ,because java method arg can't end with ", "
+      subTypeString = subTypeString.substring(0, subTypeString.length - 2);
+    }
+    subTypeString += ">";
+    return subTypeString;
+  }
+
+  static String getTypeString(Property property,
+      {bool convertToClass = false}) {
+    String typeString = "";
+    var baseType = baseTypeMap[property.type];
+    var classType = classTypeMap[property.type];
+    var specialType = specialTypeMap[property.type];
+    if (baseType != null) {
+      if (convertToClass) {
+        typeString += "NSNumber *";
+      } else {
+        typeString += baseType;
+      }
+    } else if (classType != null) {
+      if (!classType.isEmpty) {
+        typeString += "$classType";
+      }
+      if (property.subType.isNotEmpty) {
+        String subTypeString = getSubTypeString(property);
+        typeString += subTypeString;
+      }
+      typeString += " *";
+    } else if (specialType != null) {
+      typeString = getTypeString(property.subType.first);
+    } else {
+      typeString += "$prefix${property.type.split(".").last} *";
+    }
+    return typeString;
   }
 
   /// save all content use shell
@@ -110,99 +266,5 @@ class ObjectiveCCreate {
         ['-c', "echo '${content.replaceAll("'", "\'\"\'\"\'")}' >> $path"],
         runInShell: true);
     print("file: $path \n create result: ${a.exitCode}");
-  }
-
-  // /// add import
-  // static void _addObjectImport(RegExp exp, List<String> map, int index,
-  //     String packageName, List<String> imports) {
-  //   String str1 = "";
-  //   try {
-  //     str1 = exp.firstMatch(map[index]).group(0);
-  //     map[index] = "new " + map[index];
-  //   } catch (e) {}
-  //   String str2 =
-  //       "import $packageName.${str1.substring(0, str1.indexOf("("))};\n";
-  //   if (!imports.contains(str2)) {
-  //     imports.add(str2);
-  //   }
-  // }
-
-  /// create method
-  static String method(List<MethodInfo> methods) {
-    String result = "";
-    methods.forEach((method) {
-      String argType = "";
-      method.args.forEach((arg) {
-        argType += getTypeStr(arg) + " " + arg.name + ", ";
-      });
-      if (argType.endsWith(", ")) {
-        //remove ", " ,because java method arg can't end with ", "
-        argType = argType.substring(0, argType.length - 2);
-      }
-      String body = ";";
-      //don't support no abstract method
-      // if (method.isAbstract) {
-      //   body = ";";
-      // } else {
-      //   body = "{}";
-      // }
-      result += "\t" +
-          getTypeStr(method.returnType) +
-          " " +
-          method.name +
-          "(" +
-          argType +
-          ")$body\n";
-    });
-    return result;
-  }
-
-  /// cover dart type to java type
-  /// [wantAddPre] if true, it when add pre keywords, like: public static final .....
-  static String getTypeStr(
-    Property property, {
-    bool wantAddPre = false,
-    bool showNullTag = true,
-  }) {
-    String typeStr = "@property (nonatomic, ";
-    var baseType = baseTypeMap[property.type];
-    var classType = classTypeMap[property.type];
-    if (baseType != null) {
-      //base
-      typeStr += "assign) " + baseType;
-    } else if (classType != null) {
-      typeStr += "strong";
-      if (showNullTag && typeStr != "void") {
-        typeStr += (property.canBeNull ? ", nullalbe) " : ") ") + classType;
-      }
-    } else {
-      //other not base type
-      typeStr = property.type.split(".").last;
-    }
-    // if (property.subType.isNotEmpty) {
-    //   typeStr += "<";
-    //   property.subType.forEach((element) {
-    //     typeStr += getTypeStr(element, showNullTag: false) + ", ";
-    //   });
-    //   if (typeStr.endsWith(", ")) {
-    //     //remove ", " ,because java method arg can't end with ", "
-    //     typeStr = typeStr.substring(0, typeStr.length - 2);
-    //   }
-    //   typeStr += ">";
-    // }
-    // if (wantAddPre) {
-    //   if (property.isConst) {
-    //     typeStr = " final " + typeStr;
-    //   }
-    //   if (property.isStatic) {
-    //     typeStr = " static " + typeStr;
-    //   }
-    //   if (property.isPrivate) {
-    //     typeStr = " private " + typeStr;
-    //   } else {
-    //     typeStr = " public " + typeStr;
-    //   }
-    // }
-    return typeStr;
   }
 }
